82c84767f3973c2ec9003691918a2ced7624304d
alexanderduerre (Alexander Dürre) <alexander.duerre@udo.edu>
2021-09-15 12:53:25 +0000

    changed little mistake
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/robts@239 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/armarob.R
+++ pkg/R/armarob.R
@@ -3,8 +3,6 @@ n <- length(x)
 xfreq <- frequency(x)
 arest <- arrob.filter(x,order.max=4*(arorder+maorder),aic=TRUE)
 if (aic==FALSE) {
-    arorderv <- arorder
-    maorderv <- maorder
     if (length(arest$ar)==0) {startpar <- c(rep(0,arorder+maorder))} else{
 
         if (arorder>0) {
@@ -16,14 +14,18 @@ if (aic==FALSE) {
             fu <- function(par) {
                 res <- numeric(arorder+maorder)
                 for (i in 1:arorder) {
-                    res[i] <- ARMAacf(ar=par[1:arorder],ma=par[(arorder+1):(arorder+maorder)],lag.max=i)[i+1]-acf1[i]
+                    tryacf <- try(tacvfARMA(phi=par[1:arorder],theta=-par[(arorder+1):(arorder+maorder)],maxLag=i),silent=TRUE)
+                    if (inherits(tryacf,"try-error")) return(Inf)
+                    res[i] <- tryacf[i+1]/tryacf[1]-acf1[i]
                     }
                 for (i in 1:maorder) {
                     res[arorder+i] <- acfu(i,ar=par[1:arorder],ma=par[(arorder+1):(arorder+maorder)])-acf2[i]
                     }
                 return(sum(abs(res)))
                 }
-            startpar <- optim(rep(0,arorder+maorder),fu)$par
+            arstart <- rep(0,arorder)
+            if (arest$order>0) arstart[1:min(arest$order,arorder)] <- arest$ar[1:min(arest$order,arorder)]
+            startpar <- optim(c(arstart,rep(0,maorder)),fu)$par
         } else {
 
             acf2 <- numeric(maorder)
@@ -36,11 +38,13 @@ if (aic==FALSE) {
         }
     if(arorder>0) {
         fu2 <- function(par) {
-            residualv <- filterrob.statespaceARMA(x, par[1:arorder], par[(arorder+1):(arorder+maorder)], arest$var.pred, median, psi.l = 2, psi.0 = 3, na.action = na.fail)$residuals[(1+max(arorder,maorder+1)):n]
+            residualv <- try(filterrob.statespaceARMA(x, par[1:arorder], par[(arorder+1):(arorder+maorder)], arest$var.pred, median, psi.l = 2, psi.0 = 3, na.action = na.fail)$residuals[(1+max(arorder,maorder+1)):n],silent=TRUE)
+            if(inherits(residualv,"try-error")) return(Inf)
             return(scaleTau2(residualv))
             }
 
-        estpar <- optim(startpar,fu2)
+        estpar <- try(optim(startpar,fu2),silent=TRUE)
+        if(inherits(residualv,"try-error")) {estpar <- optim(rep(0,arorder+maorder),fu2)}
         filtered <- filterrob.statespaceARMA(x, estpar$par[1:arorder], estpar$par[(arorder+1):(arorder+maorder)], estpar$value, median, psi.l = 2, psi.0 = 3, na.action = na.fail)
         aictable <- 2*log(estpar$value)+ aicpenalty(arorder+maorder+1)/(n-arorder-maorder)
         var.predv <- estpar$value
@@ -75,11 +79,18 @@ if (aic==FALSE) {
     } else {
         aictable <- matrix(ncol=maorder+1,nrow=arorder+1)
         aictable[,1] <- arest$aic[1:(arorder+1)]
+        maestv <- NULL
+        arestv <- arest$ar
+        var.predv <- arest$var.pred
+        
         for (i1 in 1:maorder) {
             for (j1 in 0:arorder) {
               
                 if (j1>0) {
-                    acf1 <- ARMAacf(ar=arest$ar,lag.max=j1)[(1:j1)+1]
+                    if(length(arest$ar)>0) {
+                        acf1 <- ARMAacf(ar=arest$ar,lag.max=j1)[(1:j1)+1]
+                        } else {acf1 <- rep(0,j1)} 
+    
                     acf2 <- numeric(i1)
                     for (i in 1:i1) {
                         acf2[i] <- cov(arest$x[(i+1):n],M_psi(arest$resid[1:(n-i)]/sqrt(arest$var.pred), type = "smooth", k = c(2, 3)),use="complete.obs")/sqrt(arest$var.pred)*1.184
@@ -87,14 +98,20 @@ if (aic==FALSE) {
                     fu <- function(par) {
                         res <- numeric(j1+i1)
                         for (i in 1:j1) {
-                            res[i] <- ARMAacf(ar=par[1:j1],ma=par[(j1+1):(j1+i1)],lag.max=i)[i+1]-acf1[i]
+                            tryacf <- try(tacvfARMA(phi=par[1:j1],theta=-par[(j1+1):(j1+i1)],maxLag=i),silent=TRUE)
+                            if (inherits(tryacf,"try-error")) return(Inf)
+                            res[i] <- tryacf[i+1]/tryacf[1]-acf1[i]
                             }
                         for (i in 1:i1) {
                             res[j1+i] <- acfu(i,ar=par[1:j1],ma=par[(j1+1):(j1+i1)])-acf2[i]
                             }
                     return(sum(abs(res)))
                     }
-                startpar <- optim(rep(0,arorder+maorder),fu)$par
+                arstart <- rep(0,j1)
+                mastart <- rep(0,i1)
+                if (length(arestv)>0) arstart[1:min(j1,length(arestv))] <- arestv[1:min(j1,length(arestv))]
+                if (length(maestv)>0) mastart[1:min(j1,length(maestv))] <- maestv[1:min(j1,length(maestv))]
+                startpar <- optim(c(arstart,mastart),fu)$par
                 } else {
 
                     acf2 <- numeric(i1)
@@ -106,12 +123,14 @@ if (aic==FALSE) {
             
                 if(j1>0) {
                     fu2 <- function(par) {
-                        residualv <- filterrob.statespaceARMA(x, par[1:j1], par[(j1+1):(j1+i1)], arest$var.pred, median, psi.l = 2, psi.0 = 3, na.action = na.fail)$residuals[(1+max(j1,i1+1)):n]
+                        residualv <- try(filterrob.statespaceARMA(x, par[1:j1], par[(j1+1):(j1+i1)], arest$var.pred, median, psi.l = 2, psi.0 = 3, na.action = na.fail)$residuals[(1+max(j1,i1+1)):n],silent=TRUE)
+                        if (inherits(residualv,"try-error")) return(Inf)
                         return(scaleTau2(residualv))
                         }
 
-                    estpar <- optim(startpar,fu2)
-                    aicv <- 2*log(estpar$value)+ aicpenalty(j1+i1+1)/(n-j1-i1)
+                    try(estpar <- optim(startpar,fu2),silent=TRUE)
+                    if (inherits(estpar,"try-error")) {aicv <- Inf} else {
+                    aicv <- 2*log(estpar$value)+ aicpenalty(j1+i1+1)/(n-j1-i1)}
                     if (min(aictable,na.rm=TRUE) > aicv) 
                         {
                         arestv <- estpar$par[1:j1]
@@ -156,12 +175,12 @@ if (aic==FALSE) {
                     }
             }
         }
-    rownames(aictable) <- paste("AR-order ", 1:3,":",sep="")
-    colnames(aictable) <- paste("MA-order ", 1:3,":",sep="")
-    if ((arorderv==0)&(maorderv>0)) residualv <- filterrob.statespaceARMA(x, 0, maestv, var.predv, median, psi.l = 2, psi.0 = 3, na.action = na.fail)$residuals
-    if ((arorderv>0)&(maorderv>0)) residualv <- filterrob.statespaceARMA(x, arestv, maestv, var.predv, median, psi.l = 2, psi.0 = 3, na.action = na.fail)$residuals
-    if ((arorderv=0)&(maorderv=0)) residualv <- x-median(x)
-    if ((arorderv>0)&(maorderv==0)) residualv <- filterrob.statespace(x, arestv, var.predv, median, psi.l = 2, psi.0 = 3, na.action = na.fail)$residuals
+    rownames(aictable) <- paste("AR-order ", 0:2,":",sep="")
+    colnames(aictable) <- paste("MA-order ", 0:2,":",sep="")
+    if ((length(arestv)==0)&(length(maestv)>0)) residualv <- filterrob.statespaceARMA(x, 0, maestv, var.predv, median, psi.l = 2, psi.0 = 3, na.action = na.fail)$residuals
+    if ((length(arestv)>0)&(length(maestv)>0)) residualv <- filterrob.statespaceARMA(x, arestv, maestv, var.predv, median, psi.l = 2, psi.0 = 3, na.action = na.fail)$residuals
+    if ((length(arestv)==0)&(length(maestv)==0)) residualv <- x-median(x)
+    if ((length(arestv)>0)&(length(maestv)==0)) residualv <- filterrob.statespace(x, arestv, var.predv, median, psi.l = 2, psi.0 = 3, na.action = na.fail)$residuals
     }
 
   res <- list(
@@ -169,7 +188,7 @@ if (aic==FALSE) {
 		ar = arestv,
         ma = maestv,
 		var.pred = var.predv^2,
-		x.mean = median(filtered$filtered),
+		x.mean = median(x),
 		x.intercept = NULL,
         aic = aictable,
 		n.used = n,
@@ -181,7 +200,7 @@ if (aic==FALSE) {
 		x = x
 	)
 	attr(res, "na.action") <- attr(x, "na.action")
-	class(res) <- c("arrob", "arma")
+	class(res) <- c("armarob")
 	return(res)
 }
 #if (asyvar==TRUE)	{
